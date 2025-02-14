5c10f2cf2a8b11ecbdf2397224bcbc6b1c7243a1
Xiao Ma <Your>
2025-02-14 10:11:23 +0700

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/showname')
+  showName(): string {
+    return this.appService.getName();
+  }
+   
+  @Get('/showinformation')
+  showInfo(): string {
+    return this.appService.showInfo();
+  }
+
+  @Get('/showJson')
+  getJSON(){
+    return this.appService.getJSON();
+  }
+
+
+
+
+}
---
+++ src/app.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'We love NestJS';
+  }
+
+  getName():string{
+    return'Xiao Ma';
+  }
+
+  showInfo():string{
+    return'I am Xiao Ma ,19 years old';
+  }
+  
+  getJSON(){
+    return{
+     name:'Xiao' ,
+     lastname:'Ma',
+     age:19,
+    }
+   }
+ 
+
+
+  myJOSN(){
+   return{
+    name:'Xiao' ,
+    lastname:'Ma',
+    age:19,
+   }
+
+  
+}
+}
